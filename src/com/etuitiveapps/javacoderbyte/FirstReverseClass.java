bda62899acdb3ccafdf2e822327d19f00be928f3
Etuitive Apps <namoricoe@gmail.com>
2015-04-06 21:55:47 -0500

    user coderstyle to improve java style of the firstreverse class

--- src/com/etuitiveapps/javacoderbyte/FirstReverseClass.java
+++ src/com/etuitiveapps/javacoderbyte/FirstReverseClass.java
@@ -1,16 +1,20 @@
 package com.etuitiveapps.javacoderbyte;
 
 public class FirstReverseClass {
-	
-	public String FirstReverse(String str) { 
-		 StringBuilder sb = new StringBuilder();
-	        char[] charArray = str.toCharArray();
-	        int startIndex = str.length() - 1;
-	        for (int i = startIndex; i >= 0; i--) {
-	            sb.append(charArray[i]);
-	        }
-	        return sb.toString();
-	    
-	  } 
-
+/**
+ * Using the Java language, have the function FirstReverse(str)
+ *  take the str parameter being passed and return the string in reversed order.
+ *  Use the Parameter Testing feature in the box below to test your code with different arguments.
+ * @param str is the user input.
+ * @return Reverse of the user input.
+ */
+  public String firstReverse(String str) {
+    StringBuilder sb = new StringBuilder();
+    char[] charArray = str.toCharArray();
+    int startIndex = str.length() - 1;
+    for (int i = startIndex; i >= 0; i--) {
+      sb.append(charArray[i]);
+    }
+    return sb.toString(); 
+  }
 }
